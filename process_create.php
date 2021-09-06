e0eff5d5f684372d43db246a9dee2414be8201f8
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-09-07 02:12:00 +0900

    테이블 명 변경

--- process_create.php
+++ process_create.php
@@ -7,7 +7,7 @@ $filtered = array(
     'author_id'=>mysqli_real_escape_string($conn, $_POST['author_id'])
 );
 $sql = "
-  INSERT INTO topic
+  INSERT INTO atest
     (title, description, created, author_id)
     VALUES(
         '{$filtered['title']}',
@@ -19,7 +19,9 @@ $sql = "
 $result = mysqli_query($conn, $sql);
 if($result === false){
     echo '저장하는 과정에서 문제가 생겼습니다. 관리자에게 문의해주세요';
+    //echo mysqli_error($conn);
     error_log(mysqli_error($conn));
+    //사용자에게 error의 원인을 보여주면 안됨
 } else {
     echo '성공했습니다. <a href="index.php">돌아가기</a>';
 }
